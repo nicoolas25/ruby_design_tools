658b8aee950b2f954aea7ea7b6fa31f37a2cb9ad
Nicolas ZERMATI <nicoolas25@gmail.com>
2013-07-14 22:58:17 +0200

    add keywords to the decorator class and benchmark them

--- benchmark/decorator_bm.rb
+++ benchmark/decorator_bm.rb
@@ -10,12 +10,32 @@ class Subject
   end
 end
 
-n         = ENV['N'] || 5000000
-subject   = Subject.new
-decorated = RubyDesignTools::Decorator.new(Subject.new)
+class SubjectDecorator < RubyDesignTools::Decorator
+  def method
+    subject.method
+  end
+end
+
+class SubjectDecoratorRedirectDSL < RubyDesignTools::Decorator
+  redirect :method
+end
+
+class SubjectDecoratorAccessorDSL < RubyDesignTools::Decorator
+  accessor :method
+end
+
+n        = ENV['N'] || 5000000
+subject  = Subject.new
+proxied  = RubyDesignTools::Decorator.new(Subject.new)
+explicit = SubjectDecorator.new(Subject.new)
+redirect = SubjectDecoratorRedirectDSL.new(Subject.new)
+accessor = SubjectDecoratorAccessorDSL.new(Subject.new)
 
 
 Benchmark.bmbm do |b|
-  b.report('direct')    { n.times { subject.method } }
-  b.report('decorated') { n.times { decorated.method } }
+  b.report('direct call')          { n.times { subject.method } }
+  b.report('redefinition')         { n.times { explicit.method } }
+  b.report('DSL accessor')         { n.times { accessor.method } }
+  b.report('DSL redirect')         { n.times { redirect.method } }
+  b.report('method_missing')       { n.times { proxied.method } }
 end
--- lib/ruby_design_tools/decorator.rb
+++ lib/ruby_design_tools/decorator.rb
@@ -7,7 +7,29 @@ module RubyDesignTools
     end
 
     def method_missing(method_name, *args, &block)
-      @subject.send(method_name, *args, &block)
+      @subject.__send__(method_name, *args, &block)
+    end
+
+    class << self
+      def redirect(*methods)
+        methods.each do |method|
+          class_eval %Q{
+            def #{method}(*args, &block)
+              @subject.__send__(:#{method}, *args, &block)
+            end
+          }
+        end
+      end
+
+      def accessor(*methods)
+        methods.each do |method|
+          class_eval %Q{
+            def #{method}
+              @subject.#{method}
+            end
+          }
+        end
+      end
     end
   end
 end
